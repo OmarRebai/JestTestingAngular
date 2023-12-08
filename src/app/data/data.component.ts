bd62dcb3f8aa1933bf5f1e224aa9a0379c3c1190
Sellami-ahmed <ahmed.sellami@enis.tn>
2023-12-08 18:16:47 +0100

    j

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { FakeService } from './../services/fake.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
-export class DataComponent {
-
-}
+export class DataComponent implements OnInit {
+  errorMessage:any;
+  serviceData:any;
+  constructor(private FakeService:FakeService) { }
+  
+  ngOnInit(): void {
+    this.getServiceData();}
+    getServiceData() {
+    this.FakeService.getDataV1().subscribe(
+      {
+    next: data =>{this.serviceData=data;},
+    error: err => {this.errorMessage=err.statusText;},
+    complete: () => {
+      console.log("Finished");
+    }});
+}}
